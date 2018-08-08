2d8f5d1ff4d9553138ee7c1180f2a23a9747dd93
Zaky Hussein <afrozaky@gmail.com>
2018-08-08 16:52:10 -0400

    Update flexiblePump.ino
    Clarified variable names and added some more comments for them

--- flexiblePump.ino
+++ flexiblePump.ino
@@ -1,6 +1,6 @@
-/** LINEAR STEPPER MOTOR ACCELERATION
+/** Flexible Pump Platform
     Authors       : Zaky Hussein, Priyanka Deo
-    Last Modified : June 26th, 2018
+    Last Modified : August 8th, 2018
     This program does the following:
     -> Measures the frequency of EMF cycles
     -> Synchronizes the working fluid cycle with the EMF cycle
@@ -8,8 +8,8 @@
     then decelerates it back to a minimum frequency. Then it changes the direction of rotation and repeats the sequence a given number of times.
 
     Please follow the folllowing instructions for running the code:
-    1) Turn the sine EMF ON
-    2) Hit the upload button on the screen
+    1) Turn the sine EMF ON by increasing the temperature to 37 (GE magnetic refigeration prototype)
+    2) Hit the upload button on the screen (the arrow next to the checkmark right beneath edit)
     3) Select Tools -> Serial Monitor
     4) Turn motor driver ON when indicated
     5) Follow remaining instructions on the serial monitor
@@ -19,40 +19,40 @@
 // INITIALIZING - Declaring inputs and constants
 //-------------------------------------
 
-// Part I - IR SENSOR VARIABLES
-#define interruptPin 3                                                              // tachometer limit switch pin on Arduino board
+// Input/Output Pins
+const byte interruptPin = 3;                                                           //pin for limit switch mounted on the rotating magnetic drum
+const byte DIR = 9;                                                                 //direction pin on Arduino board
+const byte CLOCK = 10;                                                              //clock pin on Arduino board
+const byte LIMIT = 12;                                                              //home limit switch pin on Arduino board
+const byte ONOFF = 2;                                                               //On off pin on Arduino board, HIGH turns off the driver
+
 double fluxRPM = 0;
 double fluxFreq = 0;
 
-// Part II - USER INPUTS
-double strokeLength = 0;                                                            // [mm] stroke length to cover in a half cycle
-double cycles = 0;
-int speedSetting = 0;
+//USER INPUTS
+double strokeLength = 0;                                                            //[mm] stroke length to cover in a half cycle
+double cycles = 0;                                                                  //Number of cycles to run the pump for (one cycle means moving the stroke length away from home and back to home)
+int speedSetting = 0;                                                               //Current speed setting of GE prototype
 
-// Part III - DECLARING CONSTANT VARIABLES
-double peakTime;
-const double startStepFreq = 250;                                                   // [Hz] min value of 62 Hz by precision of delayMicroseconds
-double finalStepFreq;
-double finalStepPeriod;
-double startStepPeriod;
+//Stepper Motor Motion Variables
+double peakTime;                                                                    //Time for which the max frequency will be held
+const double startFreq = 250;                                                   // [Hz] the start/stop frequency, minimum value of 62 Hz by precision of delayMicroseconds function
+double maxFreq;                                                                     // [Hz] the max frequency attained
+double maxPeriod;                                                                   // [us] associated period with maxFreq
 const double df = 135;                                                              // [Hz] frequency increments
 unsigned int dt = 2950;                                                             // [us] time increments
-const byte DIR = 9;                                                                 // direction pin on Arduino board
-const byte CLOCK = 10;                                                              // clock pin on Arduino board
-const byte LIMIT = 12;                                                              // limit switch pin on Arduino board
-const byte ONOFF = 2;                                                               //On off pin on Arduino board, HIGH turns off the driver
-double nPulses;
-double constPulses;
-int counter = 0;
+double rampSteps;                                                                   // the number of steps (or pulses) that should be covered during one ramp up or one ramp down                                         
+double constSteps;                                                                  // the number of steps (or pulses) that should be sent at the constant max frequency
 double totalTime;                                                                   // [s] time for one half cycle/full stroke length
 double travelTime;
 
-// Part IV - NON-CONSTANT VARIABLES THAT UPDATE WITH TIME
-unsigned long initialTime;
-double freq = startStepFreq;
-unsigned long stepCount = 0;
-unsigned long totSteps = 0;
-unsigned long initialTime2;
+//Variables that change throughout the code
+unsigned long initialTime;                                                          //Saves an initial time that can be used later to calculate time passed
+double freq;                                                                        //The current frequency being outputted to the stepper motor
+unsigned long stepCount = 0;                                                        //Used to count steps throughout frequency profile
+unsigned long totalSteps = 0;
+int counter = 0;
+unsigned long initialTime2;                                                         
 unsigned long totalTimebegin;
 double period;
 
@@ -132,17 +132,16 @@ void loop() {
     if (speedSetting == 1) {
       travelTime = 0.76 * totalTime;                        //Decrease travel time to increase speed at speed setting 1 to stay within reliable operating frequency of stepper motor
     } else if (speedSetting == 2) {
-      travelTime = 0.85 * totalTime;                  //Need to decrease travel time less at higher speed settings
+      travelTime = 0.9 * totalTime - 0.12;                  //Need to decrease travel time less at higher speed settings
     }
 
     //Initialize variables from user input
     peakTime = sqrt(pow(travelTime, 2) - 3840 * strokeLength / pow(10, 6));        // [s] time for which max frequency should be maintained
-    finalStepFreq = pow(10, 6) * (travelTime - peakTime) / 48;
-    finalStepPeriod = 1 / (finalStepFreq) * pow(10, 6);                           // [us] final period corresponding to maximum frequency
-    startStepPeriod = 1 / (startStepFreq) * pow(10, 6);                           // [us] base period or the period to ramp up from and ramp down to
-    nPulses = ((finalStepFreq - startStepFreq) / 2.0) * .024 * finalStepFreq * .001;
-    constPulses = finalStepFreq * peakTime;
-    period = startStepPeriod;
+    maxFreq = pow(10, 6) * (travelTime - peakTime) / 48;
+    maxPeriod = 1 / (maxFreq) * pow(10, 6);                           // [us] period corresponding to maximum frequency
+    rampSteps = ((maxFreq - startFreq) / 2.0) * .024 * maxFreq * .001;
+    constSteps = maxFreq * peakTime;
+    period = 1 / (startFreq) * pow(10, 6);                           // [us] Initialize period to start/stop period
 
     if (speedSetting > 2 || speedSetting < 0) {
       Serial.println("Please enter a speed setting in the range of 1 to 2");
@@ -156,11 +155,11 @@ void loop() {
       delay(500);
     }
 
-    if (finalStepFreq > 5200) {
+    if (maxFreq > 5200) {
       Serial.println("Entered stroke length yields a max frequency > 5200, enter a decreased stroke length");
       strokeLength = cycles = 0;
       delay(500);
-    } else if (finalStepFreq < 500) {
+    } else if (maxFreq < 500) {
       Serial.println("Entered stroke length yields a max frequency < 500, enter an increased stroke length");
       strokeLength = cycles = 0;
       delay(500);
@@ -178,9 +177,9 @@ void loop() {
       Serial.print("Average Stroke Frequency: ");
       Serial.println(1.0 / (2.0 * totalTime));
       Serial.print("Max Stepper RPM: ");
-      Serial.println(finalStepFreq / 200.0 * 60);
+      Serial.println(maxFreq / 200.0 * 60);
       Serial.print("Max Stepper Frequency: ");
-      Serial.println(finalStepFreq);
+      Serial.println(maxFreq);
       Serial.println(cycles * 1.0 / fluxFreq);
       Serial.println("Beginning motor control sequence in 3 seconds");
       homeleft();                                        // bring pistons to home position before starting cycles
@@ -194,10 +193,10 @@ void loop() {
   while (counter < 2 * cycles) {
     totalTimebegin = millis();
     stepCount = 0;
-    totSteps = 0;
+    totalSteps = 0;
     initialTime2 = millis();
     // RAMP UP TO MAX FREQUENCY
-    while (stepCount < nPulses && freq < finalStepFreq) {
+    while (stepCount < rampSteps && freq < maxFreq) {
       initialTime = micros();
       while (micros() - initialTime < dt) {
         digitalWrite(CLOCK, HIGH);
@@ -205,20 +204,20 @@ void loop() {
         digitalWrite(CLOCK, LOW);
         delayMicroseconds(period / 2.0);
         stepCount++;
-        if (stepCount >= nPulses)
+        if (stepCount >= rampSteps)
           break;
       }
       freq += df;
       period = 1 / freq * pow(10, 6);
     }
-    totSteps += stepCount;
+    totalSteps += stepCount;
     //Serial.println(millis()-initialTime2);
     //Serial.println(freq);
 
     // REMAIN AT MAX FREQUENCY
     stepCount = 0;
-    freq = finalStepFreq;
-    for (int i = 0; i < constPulses; i++) {
+    freq = maxFreq;
+    for (int i = 0; i < constSteps; i++) {
       digitalWrite(CLOCK, HIGH);
       delayMicroseconds(period / 2.0);
       digitalWrite(CLOCK, LOW);
@@ -226,10 +225,10 @@ void loop() {
       stepCount++;
     }
 
-    totSteps += stepCount;
+    totalSteps += stepCount;
     // RAMP DOWN FROM MAX FREQUENCY
     stepCount = 0;
-    while (freq > startStepFreq)  {
+    while (freq > startFreq)  {
       initialTime = micros();
       while (micros() - initialTime < dt) {
         digitalWrite(CLOCK, HIGH);
@@ -241,7 +240,7 @@ void loop() {
       freq -= df;
       period = 1 / freq * pow(10, 6);
     }
-    totSteps += stepCount;
+    totalSteps += stepCount;
 
     /*if (counter % 2 == 1) {                                                             //Drift correction
       if (digitalRead(LIMIT) == LOW) {
@@ -269,7 +268,7 @@ void loop() {
     else {
       //      initialTime = millis();
       waitTime();
-      Serial.println(totSteps);
+      Serial.println(totalSteps);
       //      Serial.println("Wait Time: ");
       //      Serial.println(millis() - initialTime);
       //      Serial.println("Total Time: ");
@@ -281,4 +280,3 @@ void loop() {
   }
 
 }
-
